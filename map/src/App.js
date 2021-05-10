2cf78e9f4adc7174e5704db0929c3390340a074e
fahmialis <fsinergia@yahoo.com>
2021-05-10 20:35:01 +0700

    revisi zoom dan center point

--- map/src/App.js
+++ map/src/App.js
@@ -4,9 +4,8 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import markers from './data/marker.json'
 
 function App() {
-  console.log(markers);
   return (
-    <MapContainer center={[36.909503, 126.69407466]} zoom={13} scrollWheelZoom={true}>
+    <MapContainer center={[33.443011, -112.556876]} zoom={5} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
