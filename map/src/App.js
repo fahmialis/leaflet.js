bd28b8bf55c1594454fb17eb9ad3c29e46f2d371
fahmialis <fsinergia@yahoo.com>
2021-05-10 20:57:13 +0700

    filter station on UK only for performance

--- map/src/App.js
+++ map/src/App.js
@@ -2,23 +2,36 @@
 import './App.css';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import markers from './data/marker.json'
+import teslaData from './data/tesla-station.json'
 
 function App() {
+  const UKStations = teslaData.filter(tesla => tesla.address.country === 'United Kingdom')
   return (
-    <MapContainer center={[33.443011, -112.556876]} zoom={5} scrollWheelZoom={true}>
+    <MapContainer center={[53.314145,-1.281904]} zoom={8} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       {
-        markers.map(marker => {
+        UKStations.map(tesla => {
         return <Marker 
-        key={marker.id}
-        position={[marker.gps.latitude, marker.gps.longitude]}>
-          <Popup>
-            {
-              [`${marker.gps.latitude}, ${marker.gps.longitude}`]
-            }
+        // eventHandlers={onclick={()=>click()}}
+        key={tesla.id}
+        position={[tesla.gps.latitude, tesla.gps.longitude]}>
+          <Popup position={[tesla.gps.latitude, tesla.gps.longitude]}>
+            <div>
+              <h3>{
+                `Name : ${tesla.name},
+                Address:
+                  street: ${tesla.address.street},
+                  city: ${tesla.address.city},
+                  state: ${tesla.address.state},
+                  zip: ${tesla.address.zip}
+                  country: ${tesla.address.country}
+                  region: ${tesla.address.region}
+                `
+              }</h3>
+            </div>
           </Popup>
         </Marker>
         })
